3de76a5cd5b2338bb8ff96f0438b205212aad8b8
ahmadnaseer143 <anaseer942@gmail.com>
2025-04-15 04:09:17 -0500

    auth store zustand created

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
